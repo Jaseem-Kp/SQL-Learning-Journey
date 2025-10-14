c260a0cd3e12fb50e60b873295dc8007262851c1
Jaseem-Kp <muh.jaskp@gmail.com>
2025-10-14 12:59:49 +0530

    Create Day_2_SQL_Fundamentals.sql

---
+++ Day_2_SQL_Fundamentals.sql
@@ -0,0 +1,8 @@
+-- SELECT all columns from a table
+SELECT * FROM Customers;
+-- SELECT Specific Columns from a table
+SELECT CustomerName,City FROM Customers;
+-- SELECT DISTINCT values
+SELECT DISTINCT Country FROM Customers;
+-- DISTINCT count
+SELECT COUNT(DISTINCT Counrty) FROM Customers;
