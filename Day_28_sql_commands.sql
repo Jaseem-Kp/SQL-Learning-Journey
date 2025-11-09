ce5a8e4bd51abeb6e3eddd1d87da8bee1c5e238c
Muhammed Jaseem KP <muh.jaskp@gmail.com>
2025-11-09 23:21:25 +0530

    Add SQL commands examples in Day_28_sql_commands.sql

---
+++ Day_28_sql_commands.sql
@@ -0,0 +1,29 @@
+-- SQL Commands
+-- Single line commands
+-- Example 1: Explanation command
+
+-- Selects all:
+SELECT * FROM Customers ;
+
+-- Example 2: Ignore the end of a line:
+
+SELECT * FROM Customers -- WHERE City = 'Berlin' ;
+
+-- Example 3: Ignore a statement :
+
+-- SELECT * FROM Customers ;
+SELECT * FROM Customers ;
+
+-- Multi- Line commands
+-- Example 1: As an explanation :
+
+/* Selects all columns of all the records in the Customers table:*/
+SELECT * FROM Customers ;
+
+-- Example 2: ignore many statements:
+
+/* SELECT * FROM Customers ;
+SELECT * FROM Products ;
+SELECT * FROM Orders ;
+SELECT * FROM Shippers ;*/
+SELECT * FROM Suppliers ;
