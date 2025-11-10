10ed7566bac039f34c3bee3e18e6d4e074d16b5e
Muhammed Jaseem KP <muh.jaskp@gmail.com>
2025-11-10 16:07:49 +0530

    Add SQL operators examples to Day_29_sql_operators.sql
    Added SQL operators examples including arithmetic, bitwise, comparison, compound, and logical operators.

---
+++ Day_29_sql_operators.sql
@@ -0,0 +1,54 @@
+-- SQL Operators
+-- 1: Arithmetic 
+-- Eg:
+
+SELECT 30+20;
+SELECT 30-20;
+SELECT 30*10;
+SELECT 5/200;
+SELECT 17%5;
+
+-- 2: Bitwise operator
+
+/* 
+Bitwise AND (&)
+Bitwise OR (|)
+Bitwise Exclusive OR (^) 
+*/
+
+-- 3: Comparison Operator 
+
+SELECT * FROM Products WHERE Price = 30;
+SELECT * FROM Products WHERE Price > 30;
+SELECT * FROM Products WHERE Price < 30;
+SELECT * FROM Products WHERE Price >= 30;
+SELECT * FROM Products WHERE Price <= 30;
+SELECT * FROM Products WHERE Price <> 30;
+
+-- 4: Compound Operator 
+
+/*
++= Add equals
+-= Substract equals
+*= Multiply equals
+/= Divide equals
+%= Modulo equals
+&= BitwiseAND equals
+^= Bitwiseexclusive equals
+|= BitwiseOR equals
+*/
+
+-- 4: Logical Operators:
+
+/*
+ALL
+ANY
+AND
+BETWEEN 
+EXISTS
+IN
+LIKE
+OR
+SOME 
+NOT
+*/
