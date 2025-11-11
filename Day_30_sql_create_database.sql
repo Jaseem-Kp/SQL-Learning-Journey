5b1fcce09dd68730b68019f077d98046ce58722c
Muhammed Jaseem KP <muh.jaskp@gmail.com>
2025-11-11 22:41:53 +0530

    Add SQL CREATE DATABASE statements and comments
    Added SQL statements for creating databases with examples and tips.

---
+++ Day_30_sql_create_database.sql
@@ -0,0 +1,27 @@
+-- DAY 30: SQL CREATE DATABASE STATEMENT
+-- Learning to create new databases
+
+-- SQL CREATE DATABASE STATEMENT 
+-- The CREATE DATABASE statement is used to create a new SQL database.
+
+-- Example: Create a database called "testDB":
+CREATE DATABASE testDB;
+
+/* 
+Tip: Make sure you have admin privilege before creating any database. 
+Once a database is created, you can check it in the list of databases 
+with the following SQL command: SHOW DATABASES; 
+*/
+
+-- 4. BASIC DATABASE CREATION WITH CLEANER FORMATTING
+-- Create a database for a business application
+CREATE DATABASE CompanyDB;
+
+-- 5. WHAT I'VE LEARNED TODAY:
+-- - CREATE DATABASE creates a new database
+-- - Requires admin privileges to execute
+-- - Database names should be meaningful and descriptive
+-- - SHOW DATABASES displays all available databases
+-- - This is the first step in setting up a new database system
+
+-- Next: Learning to create tables within databases!
