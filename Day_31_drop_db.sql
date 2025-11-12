f575facabb804f7b644bb6fd4629d0029f89ddde
Muhammed Jaseem KP <muh.jaskp@gmail.com>
2025-11-12 23:29:51 +0530

    Create Day_31_drop_db.sql for dropping a database
    Added SQL script to drop an existing database with usage instructions and warnings.

---
+++ Day_31_drop_db.sql
@@ -0,0 +1,10 @@
+-- SQL DROP DATABASE Statement 
+-- For drop the existing sql database
+-- Syntax : DROP DATABASE Database name;
+-- Example : Drop the database called 'testDB':
+
+DROP DATABASE testDB ;
+
+/* Be careful before dropping a database. Deleting a database will result in loss of complete information stored in the database! */
+
+/* Tip: Make sure you have admin privilege before dropping any database. Once a database is dropped, you can check it in the list of databases with the following SQL command: SHOW DATABASES; */
