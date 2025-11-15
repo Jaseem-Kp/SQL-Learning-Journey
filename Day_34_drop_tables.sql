272a4087010ab752a40b151627858c44e2ebc696
Muhammed Jaseem KP <muh.jaskp@gmail.com>
2025-11-15 17:49:13 +0530

    Add SQL DROP and TRUNCATE statements for Shippers
    Added SQL statements to drop and truncate the Shippers table.

---
+++ Day_34_drop_tables.sql
@@ -0,0 +1,11 @@
+-- SQL DROP TABLE Statement 
+-- Example: drop the existing table Shippers:
+
+DROP TABLE Shippers ;
+
+-- Be carefull before dropping a table. Deleting a table results loss of a complete informations stored in the table
+
+-- SQL TRUNCATE TABLE
+-- Example : delete the data of the Shippers table:
+
+TRUNCATE TABLE Shippers;
